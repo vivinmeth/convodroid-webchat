8a25dd49b1e391314d2a88e624eec47a79e7127d
VivinMeth <vivinmeth@gmail.com>
2021-08-06 11:27:09 +0530

    #1 [core] base setup

---
+++ packages/core/src/App.tsx
@@ -0,0 +1,26 @@
+import React, {useMemo} from 'react';
+import ReactWebChat, {createDirectLine} from "botframework-webchat";
+import './App.css';
+import {adaptiveCardsHostConfig} from "./adaptiveCardHostConfig";
+
+function App() {
+  const directline = useMemo(() => createDirectLine({
+      token: 'TwbA_KoJmQU.3-BAeeMaH_O2FvFnZ6Ez2Phc6VilHNoRvGslZ3mXmiU',
+      // domain: 'http://localhost:5003/directline'
+  }), []);
+  directline.connectionStatus$.subscribe(status => {
+    console.log('Directline -> Connnection Status:', status, directline);
+  });
+  return (
+    <div id={'convodroid__bfrwebchat'}>
+        <ReactWebChat
+            userID={'vivinmeth.lv@emplay.net'}
+            username={'vivinmeth'}
+            adaptiveCardsHostConfig={{...adaptiveCardsHostConfig}}
+            directLine={directline}
+        />
+    </div>
+  );
+}
+
+export default App;
